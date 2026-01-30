a1c8a8b1d74b07b95cd5f4e0cc7aa4fff6c9dd17
Surya R <suri624177@gmail.com>
2026-01-30 23:16:02 +0530

    appwrite

---
+++ react08/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APP_APPWRITE_URL)
+
+  return (
+    <>
+    <h1>HEYYYYYYYYYY THEEEEREEEEEE !!!!!!!!!!</h1>
+
+
+   </>
+  )
+}
+
+export default App
