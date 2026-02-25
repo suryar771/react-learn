d9c3a4a0df498e4c6d5b83a1f4e0a074f58596d6
Surya R <suri624177@gmail.com>
2026-02-25 15:07:11 +0530

    headers

--- react08/src/components/index.js
+++ react08/src/components/index.js
@@ -1,6 +1,7 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-
+import Container from "./container/Container";
+import Logo from "./Logo";
 export {
-    Header,Footer
+    Header,Footer,Logo,Container
 }
