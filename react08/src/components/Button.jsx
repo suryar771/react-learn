a803e162f66b0e150e06ce64412f62637ab9d300
Surya R <suri624177@gmail.com>
2026-02-25 18:46:34 +0530

    forwardref

--- react08/src/components/Button.jsx
+++ react08/src/components/Button.jsx
@@ -1,9 +1,19 @@
-
 import React from 'react'
 
-function Button({children}) {
+function Button({
+    children,
+    type ='button',
+    bgcolor ='bg-blue-600',
+    textColor ='text-white',
+    className ='',
+    ...props
+
+}) {
   return (
-    <Button>{children}</Button>
+    <Button className={`px-4 py-2 rounded-lg ${bgcolor} 
+    ${textColor} ${className}`} {...props}>{children}
+    
+    </Button>
   )
 }
 
